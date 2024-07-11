587e7ad038cfa1844af4a7d3631254f91ceffd6e
citec-47 <mauricendonyi40@gmail.com>
2024-07-10 19:52:57 -0700

    Connected MongoDb to our server

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const port = 4000;
 const express = require("express");
 const app = express();
-const mongoose = requre("mongoose")
-const jwt = require ("jsdwebtoken")
-const  multer = requre ("multer")
-const path  = requre ("path")
-const core = requre ("cors")
+const mongoose = requre("mongoose");
+const jwt = require ("jsdwebtoken");
+const  multer = requre ("multer");
+const path  = requre ("path");
+const core = requre ("cors");
+
+app.use (express.json());
+app.use (core());
+
+//Database connection with MongoDb
+mongoose.connect("mongodb+srv://ndonyi:ndonyi1@cluster0.pmoytxv.mongodb.net/ecommerce");
