cf03122df7f346004d526f69f3851445a31d20a8
citec-47 <mauricendonyi40@gmail.com>
2024-07-15 07:02:12 -0700

    CONNECTED UPLOAD FROM ADMIN

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Sidebar from './Components/Sidebar/Sidebar'
+import Admin from './Page/Admin/Admin'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+      <Navbar /> 
+      <Admin />
     </div>
   )
 }
--- Admin/src/Page/Admin/Admin.jsx
+++ Admin/src/Page/Admin/Admin.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './Admin.css';
-import Sidebar from '../../Components/Sidebar/Sidebar';
-import { Route, Routes } from 'react-router-dom';
-import Addproduct from '../../Components/Addproduct/Addproduct';
-import Listproduct from '../../Components/Listproduct/Listproduct';
+import React from 'react'
+import './Admin.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Addproduct from '../../Components/Addproduct/Addproduct'
 
 const Admin = () => {
   return (
-    <div className='admin'>
-      <Sidebar />
-      <Routes>
-        <Route path='/addproduct' element={<Addproduct />} />
-        <Route path='/listproduct' element={<Listproduct />} />
-      </Routes>
+    <div className='admin'> 
+        <Sidebar />
+        <Routes>
+            <Route path='/addproduct' element={<Addproduct/>}/>
+            <Route path='/productlist' element={<Addproduct/>}/>
+        </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default Admin;
+export default Admin
